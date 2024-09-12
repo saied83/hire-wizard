a65a3416761ea08fb5304b94b2aaeb6641ddbbc2
saied83 <saiedtechit@gmail.com>
2024-09-13 00:08:29 +0600

    complete logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -146,4 +146,12 @@ export const login = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "logout successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
